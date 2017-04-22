5237d48286eb9e634bd35a999311635b501a00b8
lihantao <htl00001111@163.com>
2017-04-22 19:29:24 +0800

    clothes

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20170416132333) do
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
     t.integer  "wage_upper_bound"
-    t.         "wage_lower_bound"
+    t.integer  "wage_lower_bound"
     t.string   "contact_email"
     t.boolean  "is_hidden",        default: true
   end
